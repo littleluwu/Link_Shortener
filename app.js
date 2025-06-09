566af19856f32a626fed22c310d13cdde1bfdd3a
Luwu <renan.ap.almeida@gmail.com>
2025-06-09 20:43:27 -0300

    feat: first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+// Routes
+const shortenerRoutes = require("./routes/shortener");
+const redirectRoutes = require("./routes/redirect");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Definindo as rotas
+app.use("/short", shortenerRoutes);
+app.use("/r", redirectRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Microserviço rodando na porta ${PORT}`);
+});
